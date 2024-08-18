fa7b4c4a23fb3772230e36868e46523b6616bfd8
Ugochukwu <emdeefx@gmail.com>
2024-08-18 15:28:19 +0100

    Create a function-based view in relationship_app/views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from models import Book
 
-# Create your views here.
+def book_list(request):
+    '''This view should render a simple text list of book titles and their authors.'''
+    book = Book.objects.all()
+    context = {'book_list': book}
+    return render(request, 'book/book_list.html', context)
