746d865fafab0b53d9a3fdd51e48c3f3b60f1478
Ugochukwu <emdeefx@gmail.com>
2024-08-18 15:49:40 +0100

    In views.py create class-based views

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from models import Book
+from .models import Book, Library
+from django.views.generic import ListView
 
 def book_list(request):
     '''This view should render a simple text list of book titles and their authors.'''
     book = Book.objects.all()
     context = {'book_list': book}
     return render(request, 'relationship_app/list_books.html', context)
+
+class library_view(ListView):
+    template_name = 'relationship_app/library_detail.html'
+    queryset = Library.objects.all()
