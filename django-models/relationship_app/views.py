c14324557c83d092dbac8fff1bbd4ec8964fa153
Ugochukwu <emdeefx@gmail.com>
2024-08-18 15:39:35 +0100

    In views.py correct the path to the template file in line 8.

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,4 +5,4 @@ def book_list(request):
     '''This view should render a simple text list of book titles and their authors.'''
     book = Book.objects.all()
     context = {'book_list': book}
-    return render(request, 'book/book_list.html', context)
+    return render(request, 'relationship_app/list_books.html', context)
