19a3ba5ad7b9d493a6ce2d6d8c1b4171409c02e1
Ugochukwu <emdeefx@gmail.com>
2024-08-25 22:49:32 +0100

    Setup forms.py to render a form template

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm
 
 def index(request):
     return HttpResponse("Welcome to the library")
@@ -24,3 +25,15 @@ def edit(request):
 @permission_required("bookshelf.can_delete", raise_exception=True)
 def delete(request):
     pass
+
+def get_title(request):
+    # if request is a POST request process form data.
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            return HttpResponse("Form is valid")
+        
+    # if a GET (or other methods) direct to register
+    else:
+        form = ExampleForm()
+        return render(request, "register.html")
