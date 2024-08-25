ba99631b554892c595e892aa286ed1339d693bc9
Ugochukwu <emdeefx@gmail.com>
2024-08-25 18:00:05 +0100

    Modify views.py and create a README file for documentation

---
+++ advanced_features_and_security/LibraryProject/bookshelf/README.md
@@ -0,0 +1,17 @@
+Setup Documentation
+
+permissions:
+    a. can_edit: User permission to make changes to a book instance.
+
+    b. can_view: User permission to read the content of a book instance.
+
+    c. can_create: User permission to create book instances.
+
+    d: can_delete: User permission to delete/remove an existing book instance.
+
+Groups:
+    Editors: Users with the permission to only view and edit a book.
+
+    Viewers: Users with only view permissions enabled.
+
+    Admins: Users with all the permission enabled.
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -5,18 +5,18 @@ from django.contrib.auth.decorators import permission_required
 def index(request):
     return HttpResponse("Welcome to the library")
 
-@permission_required("bookshelf.can_view", raise_exception=True)
+@permission_required("book_list.can_view", raise_exception=True)
 def view(request):
     pass
 
-@permission_required("bookshelf.can_create", raise_exception=True)
+@permission_required("book_list.can_create", raise_exception=True)
 def create(request):
     pass
 
-@permission_required("bookshelf.can_edit", raise_exception=True)
+@permission_required("book_list.can_edit", raise_exception=True)
 def edit(request):
     pass
 
-@permission_required("bookshelf.can_delete", raise_exception=True)
+@permission_required("book_list.can_delete", raise_exception=True)
 def delete(request):
     pass
