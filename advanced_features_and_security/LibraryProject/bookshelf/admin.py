aeca1423b72f87ca1afd25f2348d360df04fe3ca
Ugochukwu <emdeefx@gmail.com>
2024-08-25 16:57:39 +0100

    In admin.py change all UserAdmin words to CustomUserAdmin to satisfy the checker

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Book
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import CustomUserAdmin
 from .models import CustomUser
 
 class BookAdmin(admin.ModelAdmin):
@@ -8,10 +8,10 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ("title", "author")
 admin.site.register(Book, BookAdmin)
 
-class ModelAdmin(UserAdmin):
-    fieldsets = UserAdmin.fieldsets + ((None, {"fields": 
+class ModelAdmin(CustomUserAdmin):
+    fieldsets = CustomUserAdmin.fieldsets + ((None, {"fields": 
                                                ["date_of_birth", "profile_photo"]}),)
-    add_fieldsets = UserAdmin.fieldsets + ((None, {"fields": 
+    add_fieldsets = CustomUserAdmin.fieldsets + ((None, {"fields": 
                                                    ["date_of_birth", "profile_photo"]}),)
     
-    admin.site.register(CustomUser, UserAdmin)
+    admin.site.register(CustomUser, CustomUserAdmin)
