e16ef942fb66d9dd418f85fbb584c5189cd48429
Ugochukwu <emdeefx@gmail.com>
2024-08-25 16:30:47 +0100

    Create a custom user profile in bookshelf/models.py and perform other configurations in admin.py and settings.py

---
+++ advanced_features_and_security/bookshelf/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import User_Profile
+
+class BookAdmin(admin.ModelAdmin):
+    list_filter = ("title", "author", "publication_year")
+    search_fields = ("title", "author")
+admin.site.register(Book, BookAdmin)
+
+class ModelAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": 
+                                               ["date_of_birth", "profile_photo"]}),)
+    add_fieldsets = UserAdmin.fieldsets + ((None, {"fields": 
+                                                   ["date_of_birth", "profile_photo"]}),)
+    
+    admin.site.register(User_Profile, UserAdmin)
---
+++ advanced_features_and_security/bookshelf/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField
+
+class User_Profile(AbstractUser):
+    date_of_birth = models.DateField(input_formats=('%d/%m/%Y'))
+    profile_photo = models.ImageField()
+
+    REQUIRED_FIELDS = ["date_of_birth", "profile_photo"]
+
+class UserManager(BaseUserManager):
+    def create_user(self, date_of_birth, profile_photo, password = None):
+        '''
+        Creates and saves user with the following fields.
+        '''
+        user = self.model(
+                        date_of_birth=date_of_birth,
+                        profile_photo=profile_photo,
+                        )
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, date_of_birth, profile_photo):
+        '''
+        Create superuser with the provided DOB and profile photo
+        '''
+        user =self.create_user(
+                            date_of_birth=date_of_birth,
+                            profile_photo=profile_photo,
+                            )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
