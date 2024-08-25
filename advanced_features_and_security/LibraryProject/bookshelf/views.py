39284ac676937c5b5e9858f70d38fdec4f1752bd
Ugochukwu <emdeefx@gmail.com>
2024-08-25 18:14:32 +0100

    Modify views.py and create a README file for documentation

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,22 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import permission_required
+from .models import Book
 
 def index(request):
     return HttpResponse("Welcome to the library")
 
-@permission_required("book_list.can_view", raise_exception=True)
+@permission_required("bookshelf.can_view", raise_exception=True)
 def view(request):
-    pass
+    books = Book.objects.all()
+    context = {'book_list': books}
+    return render(request, context)
 
-@permission_required("book_list.can_create", raise_exception=True)
+
+@permission_required("bookshelf.can_create", raise_exception=True)
 def create(request):
     pass
 
-@permission_required("book_list.can_edit", raise_exception=True)
+@permission_required("bookshelf.can_edit", raise_exception=True)
 def edit(request):
     pass
 
-@permission_required("book_list.can_delete", raise_exception=True)
+@permission_required("bookshelf.can_delete", raise_exception=True)
 def delete(request):
     pass
