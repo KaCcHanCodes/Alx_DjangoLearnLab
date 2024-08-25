00c5332a89690637165dadb7512737f9db05a995
Ugochukwu <emdeefx@gmail.com>
2024-08-25 17:42:05 +0100

    Define custom permissions for books model in models.py, then enforce permissions in views

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -7,6 +7,14 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField
 
+    class Meta:
+        permissions = [
+            ('can_view', "The ability to read only"),
+            ('can_create', "The ability to make only"),
+            ('can_edit', "The ability to make changes only")
+            ('can_delete', "The ability to remove/delete only")
+        ]
+
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(input_formats=('%d/%m/%Y'))
     profile_photo = models.ImageField()
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import permission_required
 
 def index(request):
     return HttpResponse("Welcome to the library")
+
+@permission_required("bookshelf.can_view", raise_exception=True)
+def view(request):
+    pass
+
+@permission_required("bookshelf.can_create", raise_exception=True)
+def create(request):
+    pass
+
+@permission_required("bookshelf.can_edit", raise_exception=True)
+def edit(request):
+    pass
+
+@permission_required("bookshelf.can_delete", raise_exception=True)
+def delete(request):
+    pass
