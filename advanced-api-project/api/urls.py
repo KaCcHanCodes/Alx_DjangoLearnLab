e2bad69604589b67623537d3742417d55ff054b3
Ugochukwu <emdeefx@gmail.com>
2024-09-07 20:30:43 +0100

    Renamed the url paths in api/urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpattterns = [
     path('/books/', views.BookListView.as_view()),
     path('/books/<int:pk>/', views.BookDetailView.as_view()),
-    path('/create_books/', views.BookCreateView.as_view()),
-    path('/update_books/', views.BookUpdateView.as_view()),
-    path('delete_books', views.BookDeleteView.as_view()),
+    path('/books/create/', views.BookCreateView.as_view()),
+    path('/books/update/', views.BookUpdateView.as_view()),
+    path('/books/delete/', views.BookDeleteView.as_view()),
 ]
