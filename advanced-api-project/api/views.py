39f0e37f8f7e08f9f9d01dec6a7b915c5f3c62f7
Ugochukwu <emdeefx@gmail.com>
2024-09-07 20:38:14 +0100

    Apply permissions to secure API Endpoints

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,25 +3,29 @@ from .serializers import BookSerializer
 from .models import Book
 from rest_framework import mixins
 from .forms import BookForm
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
 class BookListView(generics.ListAPIView):
     '''for retrieving all books.'''
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class BookDetailView(generics.RetrieveAPIView):
     '''for retrieving a single book by ID.'''
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_object(self):
         obj = Book.objects.get(pk=self.kwargs['ID']) #retrieve a book instance by its ID
-        return obj 
+        return obj
     
 class BookCreateView(generics.CreateAPIView):
     '''for adding a new book'''
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_book(request):
         if request.method == 'POST':
@@ -34,6 +38,7 @@ class BookUpdateView(mixins.UpdateModelMixin,
     '''for modifying an existing book.'''
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
     def put(self, request, *args, **kwargs):
         return self.update(request, args, kwargs)
@@ -49,6 +54,7 @@ class BookDeleteView(mixins.DestroyModelMixin,
     '''for removing a book.'''
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, args, kwargs)
