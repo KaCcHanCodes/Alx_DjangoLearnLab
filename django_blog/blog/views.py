188f72ad39b0f09811ecd3009cb9b3d774a966ab
Ugochukwu <emdeefx@gmail.com>
2024-09-11 19:40:48 +0100

    Configure django project to serve static files

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
