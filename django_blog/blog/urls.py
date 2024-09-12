f431fdd0eacd34aa8f21074de4e232b3084a80cc
Ugochukwu <emdeefx@gmail.com>
2024-09-12 20:49:49 +0100

    Edit url path to views in blog/urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,8 +8,8 @@ urlpatterns=[
     path('profile/', views.profile, name='profile'),
     path('login/', views.customlogin, name='login'),
     path('logout/', views.customlogout, name='logout'),
-    path('posts/new/', views.postcreate.as_view(), name='create-view'),
+    path('post/new/', views.postcreate.as_view(), name='create-view'),
     path('posts/<int:pk>/',views.postdetail.as_view(), name='detail-view'),
-    path('posts/<int:pk>/edit/', views.postupdate.as_view(), name='update-view'),
-    path('posts/<int:pk>/delete/', views.postdelete.as_view(), name='delete-view'),
+    path('post/<int:pk>/update/', views.postupdate.as_view(), name='update-view'),
+    path('post/<int:pk>/delete/', views.postdelete.as_view(), name='delete-view'),
 ]
