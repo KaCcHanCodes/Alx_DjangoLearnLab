cd16180a438aee13dded16a1787e5073e1495f8f
Ugochukwu <emdeefx@gmail.com>
2024-09-13 23:26:40 +0100

    Add tagwidget to manage tags in forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post, Comment
-from taggit.managers import TaggableManager
+from taggit.forms import TagWidget
 
 class CustomUserForm(UserCreationForm):
     class Meta:
@@ -10,10 +10,10 @@ class CustomUserForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2']
 
 class PostForm(forms.ModelForm):
-    tags = TaggableManager()
     class Meta:
         model = Post
-        fields = ['title', 'content']
+        widgets = {'tags': TagWidget(),}
+        fields = ['title', 'content', 'tags']
 
 class CommentForm(forms.ModelForm):
     class Meta:
