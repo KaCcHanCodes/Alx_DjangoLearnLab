142400ebbe87c54964aefdc67e7ce9bcedbc0c29
Ugochukwu <emdeefx@gmail.com>
2024-09-20 22:49:14 +0100

    create like model in posts/models.py

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -21,5 +21,5 @@ class Comment(models.Model):
     updated_at = models.DateTimeField(auto_now_add=True)
 
 class Like(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='Likes')
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='Likes')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
